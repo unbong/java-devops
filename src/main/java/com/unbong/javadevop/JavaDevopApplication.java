e38476071dcb9bf7a7b78c974576bd8ff5e5c247
unbong <ecunbong@gmail.com>
2022-02-25 22:52:12 +0900

    java-devops first commit

---
+++ src/main/java/com/unbong/javadevop/JavaDevopApplication.java
@@ -0,0 +1,13 @@
+package com.unbong.javadevop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unbong/javadevop/JavaDevopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unbong.javadevop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
